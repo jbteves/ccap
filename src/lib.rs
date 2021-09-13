8f3a2b569248bb7a75dce3281c072744b3508a68
Joshua Teves <joshua.teves@nih.gov>
2021-09-13 17:54:44 -0400

    Partial working block header parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 /// Library to help sort out a few things
 
-use std::fmt;
+use std::{
+    fmt,
+};
 
 // Useful constants
 const MILLIS_PER_SECOND: usize = 1000;
@@ -61,13 +63,13 @@ impl SimpleTime {
     pub fn from_parts(
     hours: usize, minutes: usize, seconds: usize, milliseconds: usize) -> SimpleTime {
         if minutes >= 60 {
-            panic!("SimpleTime requires minutes be in [0, 60]");
+            panic!("SimpleTime requires minutes be in [0, 60] (got {})", minutes);
         }
         if seconds >= 60 {
-            panic!("SimpleTime requires seconds be in [0, 60]");
+            panic!("SimpleTime requires seconds be in [0, 60] (got {})", seconds);
         }
         if milliseconds >= 999 {
-            panic!("SimpleTime requires milliseconds be in [0, 999]");
+            panic!("SimpleTime requires milliseconds be in [0, 999] (got {})", milliseconds);
         }
 
         SimpleTime {
@@ -137,6 +139,171 @@ impl fmt::Display for NegativeSimpleTime {
     }
 }
 
+/// Type for parsing VTT caption files.
+/// This parser assumes a format of:
+/// - Header
+/// - Blocks of caption with
+///   - Blank Line
+///   - Line 1: Block Number
+///   - Line 2: Speaker: HH:MM:SS.mmm --> HH:MM:SS.mmm
+///     - NOTE: Speaker is optional
+///   - Line 3: Text to display for the caption
+/// and will return a Caption object when asked to parse.
+pub struct VttParser;
+
+impl VttParser {
+    /// Parse a string slice into a block number
+    fn block_number(s: &str) -> Result<usize, VttParserError> {
+        let r = s.parse::<usize>();
+        match r {
+            Ok(n) => Ok(n),
+            Err(_) => Err(VttParserError::ExpectedBlockNumber(String::from(s))),
+        }
+    }
+    /// Parse a VTT timestamp
+    fn block_timestamp(s: &str) -> Result<SimpleTime, VttParserError> {
+        // TODO: remove
+        println!("Processing {}", s);
+
+        let VTT_TIMESTAMP_LEN: usize = 12;
+        if s.len() != VTT_TIMESTAMP_LEN {
+            return Err(VttParserError::InvalidTimestamp(String::from(s)));
+        }
+        // We have correct length, parse
+        // Get hours
+        let hours = match s[0..2].parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => {
+                return Err(VttParserError::InvalidTimestamp(String::from(s)));
+            },
+        };
+        // Check first colon
+        if s.chars().nth(2).unwrap() != ':' {
+            return Err(VttParserError::InvalidTimestamp(
+                    String::from(s)));
+        }
+        // Get minutes
+        let minutes = match s[3..5].parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => {
+                return Err(VttParserError::InvalidTimestamp(String::from(s)));
+            },
+        };
+        // Check second colon
+        if s.chars().nth(2).unwrap() != ':' {
+            return Err(VttParserError::InvalidTimestamp(
+                    String::from(s)));
+        }
+        // Get seconds
+        let seconds = match s[6..8].parse::<usize>() {
+            Ok(n) => {
+                println!("{}", n);
+                n
+            },
+            Err(_) => {
+                return Err(VttParserError::InvalidTimestamp(String::from(s)));
+            },
+        };
+        // Check period
+        if s.chars().nth(8).unwrap() != '.' {
+             return Err(VttParserError::InvalidTimestamp(
+                    String::from(s)));
+        }
+        // Get milliseconds
+        let milliseconds = match s[9..12].parse::<usize>() {
+            Ok(n) => n,
+            Err(_) => {
+                return Err(VttParserError::InvalidTimestamp(String::from(s)));
+            },
+        };
+        // TODO: remove
+        println!("Got numbers: {}:{}:{}.{}", hours, minutes, seconds, milliseconds);
+
+        Ok(
+            SimpleTime::from_parts(
+                hours,
+                minutes,
+                seconds,
+                milliseconds
+            )
+        )
+    }
+    /// Parse a string slice into a tuple of block header information
+    fn block_header(s: &str) -> Result<(Option<String>, SimpleTime, SimpleTime), VttParserError> {
+        // See if we have a line to begin with
+        if s.len() == 0 {
+            return Err(VttParserError::UnexpectedEndOfFile);
+        }
+        if s.chars().nth(0).unwrap().is_numeric() {
+            // Make sure we have exactly three "words"
+            let total_words = s.split(' ').count();
+            if total_words == 3 {
+                // We're good to go, probably
+                let first = s.split(' ').nth(0);
+                let second = s.split(' ').nth(1);
+                let third = s.split(' ').nth(2);
+                if let Some(ts1) = first {
+                    if let Some("-->") = second {
+                        if let Some(ts2) = third {
+                            // Need to process the timestamps
+                            let start = VttParser::block_timestamp(ts1)?;
+                            let end = VttParser::block_timestamp(ts2)?;
+                            Ok((
+                                None,
+                                start,
+                                end))
+
+                        } else {
+                            return Err(
+                                VttParserError::InvalidTimestamp(
+                                    String::from(s)));
+                        }
+                    } else {
+                        return Err(
+                            VttParserError::InvalidTimestamp(
+                                String::from(s)));
+                    }
+                } else {
+                    return Err(VttParserError::InvalidTimestamp(
+                        String::from(s)));
+                }
+            } else {
+                return Err(
+                    VttParserError::InvalidTimestamp(String::from(s)));
+            }
+        } else {
+            // TODO: add name checking
+            panic!("In progress");
+        }
+    }
+}
+
+/// Error type for VttParser
+#[derive(Debug, Clone)]
+pub enum VttParserError {
+    UnexpectedEndOfFile,
+    ExpectedBlockNumber(String),
+    BlockHeaderInvalid(String),
+    InvalidTimestamp(String),
+}
+
+impl fmt::Display for VttParserError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VttParserError::UnexpectedEndOfFile => write!(f, "unexpected end of file"),
+            VttParserError::ExpectedBlockNumber(s) => {
+                write!(f, "expected VTT block number, got {}", s)
+            },
+            VttParserError::BlockHeaderInvalid(s) => {
+                write!(f, "invalid VTT block from line {}", s)
+            },
+            VttParserError::InvalidTimestamp(s) => {
+                write!(f, "invalid VTT block from word {}", s)
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -163,6 +330,72 @@ mod test {
             match r {
                 Ok(()) => panic!("Test failure; was okay going negative"),
                 Err(_) => assert_eq!(0, 0),
+            };
+        }
+    }
+    mod vtt_parser {
+        use super::{VttParser, VttParserError};
+        #[test]
+        fn test_parse_block_no() {
+            let n = VttParser::block_number("1").expect("");
+            assert_eq!(n, 1);
+
+            let n = VttParser::block_number("a");
+            match n {
+                Ok(_) => panic!("Test failure! VttParser parses 'a'"),
+                Err(e) => {
+                    match e {
+                        VttParserError::UnexpectedEndOfFile => {
+                            panic!("Test failure! VttParser wrong err");
+                        },
+                        VttParserError::ExpectedBlockNumber(s) => {
+                            assert_eq!(s, "a");
+                        },
+                        _ => panic!("Unknown test failure")
+                    };
+                },
+            };
+        }
+        #[test]
+        fn test_parse_block_header_no_name() {
+            // Test with no speaker listed
+            let test_str_1 = "00:00:00.000 --> 00:00:01.001";
+            let r = VttParser::block_header(test_str_1);
+            match r {
+                Ok((None, start, end)) => {
+                    assert_eq!(start.to_milliseconds(), 0);
+                    assert_eq!(end.to_milliseconds(), 1001);
+                }
+                _ => panic!("Test failed"),
+            }
+        }
+        #[test]
+        fn test_parse_block_header_with_name() {
+            // Test with speaker listed
+            let test_str_2 = "Pete Molfese 00:00:00.000 --> 00:00:01.001";
+            let r = VttParser::block_header(test_str_2);
+            match r {
+                Ok((Some(s), start, end)) => {
+                    assert_eq!(s, "Pete Molfese");
+                    assert_eq!(start.to_milliseconds(), 0);
+                    assert_eq!(end.to_milliseconds(), 1001);
+                }
+                _ => panic!("Test failed when given name"),
+            }
+        }
+        #[test]
+        fn test_parse_block_header_missing_start() {
+            // Test that we fail for no block start
+            let test_str_3 = "--> 00:00:01.001";
+            let r = VttParser::block_header(test_str_3);
+            match r {
+                Ok(_) => panic!("Things should not be okay"),
+                Err(e) => {
+                    match e {
+                        VttParserError::BlockHeaderInvalid(s) => {},
+                        _ => panic!("Test failed in unexpected way"),
+                    };
+                },
             };
         }
     }
